3c598b5d6ca404c040e0c3e2b13cf6d26b81ea1f
Trishula Software <engineering@trishulasoftware.com>
2026-04-27 16:28:51 -0400

    fix(ci): Root-cause remediation — add setup.py for Cython build, timeout for daemon scripts, mkdir telemetry

---
+++ setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="IngressForensics",
+    ext_modules=cythonize("rag_sanitizer.py", compiler_directives={'language_level': "3"}),
+    zip_safe=False,
+)
